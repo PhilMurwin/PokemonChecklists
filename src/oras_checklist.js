fa6c3bbc7bd60589e9836ec3fa4a09957903796c
Phil Murwin <pmurwin@gmail.com>
2015-01-16 22:13:29 -0600

    Move JS to separate file
    - Add id's to the data

---
+++ src/oras_checklist.js
@@ -0,0 +1,127 @@
+function GetJsonData(path)
+		{
+			var json = null;
+			$.ajax({
+				'async': false,
+				'global': false,
+				'url': path,
+				'dataType': "json",
+				'success': function(data) {
+					json = data;
+				}
+			});
+			
+			return json;
+		}
+		
+		function Render()
+		{
+			var oras = GetJsonData("data/oras_postgame_checklist.json");
+		
+			var col1 = $("#col1");
+			var col2 = $("#col2");
+			
+			var chkIndex = 0;
+			
+			for(var t=0; t < oras.oras.length; t++)
+			{
+				chkIndex++;
+				
+				var task = oras.oras[t];
+				var divTask = $("<div class='task'></div>")
+				var targetCol = col1;
+				
+				if (task.col === 2)
+				{
+					targetCol = col2;					
+				}
+				
+				//Build Task checkbox
+				var chk = BuildCheckbox(chkIndex, task);
+				divTask.html(chk);				
+				
+				// Check for subtasks
+				divTask = RenderSubTasks(divTask, chkIndex, task);
+				
+				targetCol.append(divTask);
+			}
+		}
+		
+		function RenderSubTasks(divTask, chkIndex, task)
+		{
+			var id = task.id;
+			
+			if (task.subtasks !== undefined)
+			{
+				for(var t=0; t < task.subtasks.length; t++)
+				{
+					chkIndex++;
+					
+					var subtask = task.subtasks[t];
+					subtask.parentid = id;
+					divSubtask = $("<div class='subtask'></div>")
+					
+					if (subtask.subcol !== undefined)
+					{
+						divSubtask.class('subtaskcol');
+					}
+					
+					//Build Task checkbox
+					var chk = BuildCheckbox(chkIndex, subtask);
+					divSubtask.html(chk);
+					divTask.append(divSubtask);
+					
+					// Check for subtasks
+				}
+			}
+			
+			return divTask;
+		}
+		
+		function BuildCheckbox(chkIndex, task)
+		{
+			var id = task.id;
+			var parentid = task.parentid;
+			
+			if (parentid !== undefined)
+			{
+				id = parentid + id;
+			}
+			
+			var text = task.task;
+			var formats = task.formats;
+			
+			var formattedText = text;
+			if (formats !== undefined)
+			{
+				formattedText = FormatText(formattedText, formats);
+			}
+			
+			var chk = $("<input class=\"oras_chk\" type='checkbox' id='" + id + "' /><label for='" + id + "'>" + formattedText + "</label>");
+			
+			return chk;
+		}
+		
+		function FormatText(text, formats)
+		{
+			if (formats !== undefined)
+			{
+				for(f = 0; f < formats.length; f++)
+				{
+					var all = formats[f].all;
+					var word = formats[f].word;
+					var format = formats[f].format;
+					
+					if (word !== undefined && word.length > 0)
+					{
+						text = text.replace(word,"<span class='" + format + "'>" + word + "</span>");
+					}
+					else if (all !== undefined && all.length > 0)
+					{
+						text = "<span class='" + all + "'>" + text + "</span>";
+					}
+				}
+			}
+			
+			return text;
+		}
