2dc5825fda94f4a2097571979ef8d08ceee891b3
Phil Murwin <Phil.Murwin@2gms.com>
2015-07-22 16:23:26 -0500

    Dynamically build menu via javascript
    - Add menu.json
    - Add build_menu.js
    - Remove hard coded navbar from all checklist.html files
    - Remove hard coded navbar from index.html
    - Add .gitignore to safely ignore typings folder

---
+++ src/js/build_menu.js
@@ -0,0 +1,66 @@
+/// <reference path="../../typings/jquery/jquery.d.ts"/>
+
+function GetJsonData(path)
+{
+	var json = null;
+	$.ajax({
+		'async': false,
+		'global': false,
+		'url': path,
+		'dataType': "json",
+		'success': function(data) {
+			json = data;
+		}
+		,'error': function(x, y, err) {
+			console.log(err);
+		}
+	});
+	
+	return json;
+}
+
+function BuildMenu(path)
+{
+	var navbar = $("#navbar");
+	
+	var dataKey = "menus";
+	var dataPath = (path != undefined ? "./" + path + "/" : "") + "data/menu.json";
+	var data = GetJsonData(dataPath);
+	
+	var menu = BuildMenuItems(data[dataKey], path);
+	menu = "<ul class='nav navbar-nav'>" + menu + "</ul>";
+	navbar.append(menu);
+	console.log(menu);
+}
+
+function BuildMenuItems(data, path)
+{
+	var menuItems = "";
+	
+	for(var i=0; i < data.length; i++)
+	{
+		var menuItem = data[i];
+		
+		if (menuItem["isgroup"] == 1)
+		{
+			var groupItems = BuildMenuItems(menuItem["menuitems"], path);
+			
+			var groupItem = "<li class='dropdown'>" +
+				"<a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>" + menuItem["itemname"] + "<span class='caret'></span></a>" +
+				"<ul class='dropdown-menu'>" +
+					groupItems +
+				"</ul>" +
+				"</li>";
+			
+			menuItems += groupItem;
+		}
+		else
+		{
+			var relativePath = path != undefined ? "./" + path + "/" : "";
+			var item = "<li><a href='" + relativePath + menuItem["link"] + "'>" + menuItem["itemname"] + "</a></li>";
+			menuItems += item;
+		}
+	}
+	
+	return menuItems;
+}
