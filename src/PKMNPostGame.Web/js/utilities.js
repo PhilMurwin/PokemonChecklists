c88bf843be7ec2d5cc5f103fce6e7740ee695174
PhilMurwin <Phil.Murwin@pointbsolutions.com>
2022-10-08 21:14:08 -0500

    Add a language picker to swsh page
    - add style for langpicker
    - add a french version of the swsh checklist file (no actual french yet)
    - add utilities.js with some basic useful functions

---
+++ src/PKMNPostGame.Web/js/utilities.js
@@ -0,0 +1,30 @@
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+  function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function getLang() {
+    if (navigator.languages != undefined && navigator.languages.length > 0) 
+    {
+      return navigator.languages[0];
+    }
+    return navigator.language;
+  }
